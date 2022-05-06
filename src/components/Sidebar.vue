10691dd9de2ab6c90c97cbe85d89f6f7250a6f2b
Vantol Bennett <vvantol2000@icloud.com>
2022-05-05 19:36:42 -0500

    added more components

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -161,11 +161,11 @@
     <div class="flex items-center px-4 -mx-2">
       <img
         class="object-cover mx-2 rounded-full h-9 w-9"
-        src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+        src="https://avatars.githubusercontent.com/u/99844305?v=4"
         alt="avatar"
       />
       <h4 class="mx-2 font-medium text-gray-800 dark:text-gray-200 hover:underline">
-        John Doe
+        CeoDevForce
       </h4>
     </div>
   </div>
